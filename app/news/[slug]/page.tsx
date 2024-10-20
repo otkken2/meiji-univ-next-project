a26dfeb23145e622d345084d435247e9bf10b5b5
otkken2 <lovelovezamakko2@gmail.com>
2024-10-20 16:43:14 +0900

    ニュース一覧と詳細にSSRを適用

--- app/news/[slug]/page.tsx
+++ app/news/[slug]/page.tsx
@@ -13,6 +13,8 @@ type Props = {
   }
 }
 
+export const revalidate = 0;
+
 const Page = async ({ params, searchParams }: Props) => {
   const data = await getNewsDetail(params.slug, { draftKey: searchParams.dk }).catch(notFound)
   return (
--- app/news/page.tsx
+++ app/news/page.tsx
@@ -4,7 +4,11 @@ import { NEWS_LIST_LIMIT } from "../_constants";
 import { getNewsList } from "../_libs/microcms";
 import SearchField from "../_components/SearchField";
 
+export const revalidate = 0;
+
 const Page = async () => {
+
+
   const { contents: news, totalCount } = await getNewsList({
     limit: NEWS_LIST_LIMIT
   });
